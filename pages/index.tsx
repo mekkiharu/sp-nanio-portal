afcaf0f4052dceada9571040be5ac3068fbee848
Haru Mekki <mekkiharukaze@gmail.com>
2022-11-13 00:00:41 +0800

    refactor: directory structure

--- pages/index.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div className="flex justify-center h-screen items-center">
-      <h1 className="text-3xl font-bold underline text-white">
-        Welcome to Nanio
-      </h1>
-    </div>
-  );
-}
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div className="container mx-auto flex justify-center h-screen items-center">
+      <div className={`p-12 border-white rounded-md border`}>
+        <h1 className="text-3xl font-bold underline text-white">
+          Welcome to Nanio
+        </h1>
+        <p className="text-center">This is a work in progress.</p>
+      </div>
+    </div>
+  );
+}
