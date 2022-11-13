de45f4caf9b0040cdcb5cbc1a7d4dc99ecae98be
Haru Mekki <mekkiharukaze@gmail.com>
2022-11-13 23:47:14 +0800

    chore: added head title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,20 @@
+import Head from "next/head";
+
 export default function Home() {
   return (
-    <div className="container mx-auto flex justify-center h-screen items-center">
-      <div className={`p-12 border-white rounded-md border`}>
-        <h1 className="text-3xl font-bold underline text-white">
-          Welcome to Nanio
-        </h1>
-        <p className="text-center">This is a work in progress.</p>
+    <>
+      <Head>
+        <title>Nanio</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <div className="container mx-auto flex justify-center h-screen items-center">
+        <div className={`p-12 border-white rounded-md border`}>
+          <h1 className="text-3xl font-bold underline text-white">
+            Welcome to Nanio
+          </h1>
+          <p className="text-center">This is a work in progress.</p>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
